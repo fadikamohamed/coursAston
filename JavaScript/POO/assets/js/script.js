145fb24359ef6d8658d38ce1b510facf0df6588b
fadikamohamed <fadika.mohamed02@gmail.com>
2019-10-04 15:54:58 +0200

    .

--- JavaScript/POO/assets/js/script.js
+++ JavaScript/POO/assets/js/script.js
@@ -1,29 +1,49 @@
-class Tasks{
-    constructor(inputTask, inputDateTask){
+class Tasks {
+    constructor(inputTask, inputDateTask) {
         this.inputTask = inputTask;
         this.inputDateTask = inputDateTask;
     }
-
-    getInputTask(){
-        return inputTask;
+    getInputTask() {
+        return this.inputTask;
     }
-    getInputDateTask(){
-        return inputDateTask;
+    getInputDateTask() {
+        return this.inputDateTask;
     }
-    setInputTask(){
+    setInputTask(inputTask) {
         this.inputTask = inputTask;
     }
-    setInputDateTask(){
+    setInputDateTask(inputDateTask) {
         this.inputDateTask = inputDateTask;
     }
 }
 
-function submitf (){
+let submitTask = document.getElementById('submitTask');
+submitTask.onclick = function () {
     let inputTask = document.getElementById('inputTask').value;
     let inputDateTask = document.getElementById('inputDate').value;
 
     let tasksDatas = new Tasks(inputTask, inputDateTask);
-    tasksDatas.setInputTask();
-    tasksDatas.setInputDateTask();
-    
+    tasksDatas.setInputTask(inputTask);
+    tasksDatas.setInputDateTask(inputDateTask);
+
+    let datasT = tasksDatas.getInputTask();
+    let datasD = tasksDatas.getInputDateTask();    
+    console.log(datasT);
+    console.log(datasD);
+    for (i = 0; i < datasT.length; i++) {    
+
+        let target = document.getElementById('target');
+        target.innerHTML = `
+    <tr>
+    <td scope="row">${datasT[i]}</td>
+    <td scope="row">${datasD[i]}</td>
+    <td scope="row">M</td>
+    <td scope="row">X</td>
+    </tr>
+    `
+    }
+
+
+
+
 }
